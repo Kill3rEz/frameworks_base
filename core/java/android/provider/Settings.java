269e7f7baf818938f997b371b3334dc07a587855
Michael Bestas <mkbestas@lineageos.org>
2026-08-12 21:08:15 +0000

    frameworks: Add support for advanced restart options
    Change-Id: I4b35d10b0811eff0d8e9ab68b2aedbe52b4aa53f
    
    [kenway214]
    Adapted for A17
    
    Co-Authored-By: Bruno Martins <bgcngm@gmail.com>
    Co-Authored-By: Dave Kessler <activethrasher00@gmail.com>
    Co-Authored-By: DvTonder <david.vantonder@gmail.com>
    Co-Authored-By: Jorge Ruesga <jorge@ruesga.com>
    Co-Authored-By: maxwen <max.weninger@gmail.com>
    Co-Authored-By: Ricardo Cerqueira <cyanogenmod@cerqueira.org>
    Co-Authored-By: Roman Birg <roman@cyngn.com>
    Co-Authored-By: SagarMakhar <sagarmakhar@gmail.com>
    Co-Authored-By: Veeti Paananen <veeti.paananen@rojekti.fi>
    Co-Authored-By: Zhao Wei Liew <zhaoweiliew@gmail.com>
    
    ok

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6993,6 +6993,13 @@ public final class Settings {
         public static final String GESTURE_TWO_FINGER_SWIPE = "gesture_two_finger_swipe";
 
         /**
+         * Whether to enable advanced reboot
+         * @hide
+         */
+        @Readable
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
